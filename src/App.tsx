20f3e3fd1fad033decec1df25239a38c04cd3170
Renato <rschlogel@hotmail.com>
2021-05-01 00:54:23 -0300

    criando e estilizando o composente Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+
+import { Header } from "./components/Header/header";
 import { GlobalStyle } from "./styles/global";
 
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <>
+      <Header/>
       <GlobalStyle />
-    </div>
+    </>
   );
 }
