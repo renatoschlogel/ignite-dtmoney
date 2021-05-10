4eb1ca03720e58ee812f7d2fc27245a4257d6169
renatoschlogel <renato.s@outlook.com>
2021-05-09 23:23:22 -0300

    configurando utilização do mirage

--- src/components/TransactionsTable/TransactionsTable.tsx
+++ src/components/TransactionsTable/TransactionsTable.tsx
@@ -1,6 +1,15 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "./transactionsTable.styles";
 
 export function TransactionTable () {
+
+  useEffect(() => {
+    api.get("/transactions")
+    .then(response => console.log(response.data))
+  }, []);
+
+
   return (
   <>
     <Container>
