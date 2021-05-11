572af64e12cde0368b4791ad5fb781cd17c1276f
Renato <rschlogel@hotmail.com>
2021-05-10 23:34:22 -0300

    criando um modal com a biblioteca react-modal

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,34 @@
+import { useState } from 'react';
 import logoImg from '../../assets/logo.svg';
 import { Container, Content } from './header.styles';
+import Modal from 'react-modal';
 
 
 export function Header () {
+
+  const [isNewTransactionOpen, setIsNewTransactionOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionOpen(true);
+  }
+
+  function hadleCloseNewTransactionModal() {
+    setIsNewTransactionOpen(false);
+  }
+
   return (
     <Container>
       <Content>
 
-   
+
         <img src={logoImg} alt="dt money"/>
-        <button type="button">
+        <button type="button" onClick={handleOpenNewTransactionModal}>
           Nova Transação
         </button>
+        <Modal isOpen={isNewTransactionOpen}
+               onRequestClose={hadleCloseNewTransactionModal} >
+         <h2>Cadastrar transação</h2> 
+        </Modal>
        </Content>
     </Container>
   )
