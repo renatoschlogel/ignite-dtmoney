a471676339cf0265a91f509ff773060ba02ba34a
Renato <rschlogel@hotmail.com>
2021-05-11 23:09:10 -0300

    Estrura do formlario, e criando as estilizaçoes de forma global

--- src/components/NewTransactionModal/NewTransactionModal.tsx
+++ src/components/NewTransactionModal/NewTransactionModal.tsx
@@ -1,4 +1,6 @@
+import React from 'react';
 import Modal from 'react-modal';
+import { Container } from './newTransactionModal.styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -8,8 +10,20 @@ interface NewTransactionModalProps {
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
   return (
     <Modal isOpen={isOpen}
-           onRequestClose={onRequestClose} >
-      <h2>Cadastrar transação</h2> 
+           onRequestClose={onRequestClose} 
+           overlayClassName="react-modal-overlay"
+           className="react-modal-content"
+           >
+      <Container>
+        <h2>Cadastrar transação</h2> 
+
+        <input placeholder="Titulo"/>
+        <input type="number" placeholder="Valor"/>
+        <input placeholder="Categoria"/>
+
+        <button type="submit">Cadastrar</button>
+
+      </Container>
     </Modal>
   );
 }
